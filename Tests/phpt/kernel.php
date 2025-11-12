a101e889553627f50d29e89a6161e7a240d78462
Fabien Potencier <fabien@potencier.org>
2025-11-12 12:08:48 +0100

    Update CHANGELOG for 5.4.50

---
+++ Tests/phpt/kernel.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+require __DIR__.'/autoload.php';
+
+class TestKernel implements HttpKernelInterface
+{
+    private $env;
+    private $var;
+
+    public function __construct(string $env, string $var)
+    {
+        $this->env = $env;
+        $this->var = $var;
+    }
+
+    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true): Response
+    {
+        return new Response('OK Kernel (env='.$this->env.') '.$this->var);
+    }
+}
+
+return function (array $context) {
+    return new TestKernel($context['APP_ENV'], $context['SOME_VAR']);
+};
